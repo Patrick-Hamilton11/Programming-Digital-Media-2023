6be7c2fdb2a57b95fb0d992a76a4c37d3e5d78d9
Patrick Hamilton <phamil4@lsu.edu>
2023-02-03 15:55:01 -0600

    Start of assignment 4

---
+++ Assignment4/sketch.js
@@ -0,0 +1,13 @@
+let deadBug;
+function preload(){
+  bugNormal = loadImage("assets/BugSquishNormalTransparent.png");
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  image(bugNormal,100,100, 100, 100);
+}
