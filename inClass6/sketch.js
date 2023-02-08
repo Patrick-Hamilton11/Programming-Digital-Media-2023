9a4bc7e4b73c750f244226d717d7af6f2ce39e92
Patrick Hamilton <phamil4@lsu.edu>
2023-02-07 18:27:36 -0600

    Update

--- inClass6/sketch.js
+++ inClass6/sketch.js
@@ -1,32 +1,41 @@
 let spriteSheet;
-let walkingAnimation;
+let chronoSheet;
 
-function preload(){
-  spriteSheet = loadImage('assets/SpelunkyViking.png');
+let walkingAnimation;
+let walkingAnimation2;
+
+function preload() {
+  spriteSheet = loadImage("assets/SpelunkyViking.png");
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(400, 400);
   imageMode(CENTER);
+
   walkingAnimation = new WalkingAnimation(spriteSheet,80,80,200,200,9);
+  walkingAnimation2 = new WalkingAnimation(spriteSheet,80,80,100,300,9);
 }
 
 function draw() {
   background(220);
+  
   walkingAnimation.draw();
+  walkingAnimation2.draw();
 }
 
 function keyPressed() {
-  walkingAnimation.keyPressed();
+  walkingAnimation.keyPressed(RIGHT_ARROW,LEFT_ARROW);
+  walkingAnimation2.keyPressed(LEFT_ARROW,RIGHT_ARROW);
 }
 
 function keyReleased() {
-  walkingAnimation.keyReleased();
+  walkingAnimation.keyReleased(RIGHT_ARROW,LEFT_ARROW);
+  walkingAnimation2.keyReleased(LEFT_ARROW,RIGHT_ARROW);
 }
 
-class WalkingAnimation{
-  constrctor(spriteSheet, sw, sh, dx, dy){
-    this.spriteSheet = spriteSheet;
+class WalkingAnimation {
+  constructor(spritesheet, sw, sh, dx, dy, animationLength, offsetX = 0, offsetY = 0) {
+    this.spritesheet = spritesheet;
     this.sw = sw;
     this.sh = sh;
     this.dx = dx;
@@ -35,45 +44,54 @@ class WalkingAnimation{
     this.v = 0;
     this.animationLength = animationLength;
     this.currentFrame = 0;
-    this.moving = 0
+    this.moving = 0;
     this.xDirection = 1;
+    this.offsetX = offsetX;
+    this.offsetY = offsetY;
   }
 
   draw() {
 
-    if (this.moving != 0){
-      this.u = this.currentFrame % this.animationLength;
-    }else{
-      this.u = 0;
-    }
+    // if (this.moving != 0)
+    //   this.u = this.currentFrame % this.animationLength;
+    // else
+    //   this.u = 0;
 
+    this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
+    push();
     translate(this.dx,this.dy);
-    scale(this.xDirection, 1);
-
-    image(this.spriteSheet, 0, 0, this.sw, this.sh, this.u*this.sw, this.v*this.sh,this.sw,this.sh);
-
-    if (frameCount % 4 == 0){ 
-      this.currentFrame++
+    scale(this.xDirection,1);
+  
+    image(this.spritesheet,0,0,this.sw,this.sh,this.u*this.sw+this.offsetX,this.v*this.sh+this.offsetY,this.sw,this.sh);
+    pop();
+    if (frameCount % 6 == 0) {
+      this.currentFrame++;
     }
-      this.dx += this.moving;
-
+  
+    this.dx += this.moving;
   }
 
-  keyPressed() {
-    if (keyCode === RIGHT_ARROW){
+  keyPressed(right, left) {
+    if (keyCode === right) {
       this.moving = 1;
       this.xDirection = 1;
       this.currentFrame = 1;
-    }else if (keyCode === LEFT_ARROW){
+    } else if (keyCode === left) {
       this.moving = -1;
       this.xDirection = -1;
+      this.currentFrame = 1;
     }
   }
 
-  keyReleased() {
-    if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+  keyReleased(right,left) {
+    if (keyCode === right || keyCode === left) {
       this.moving = 0;
     }
   }
-  
+
+  contains(x,y){
+    let insideX = x >= this.dx - this.sw / 2 && x <= this.dx + this.sw /2;
+    let insideY = y >= this.dy - this.sh / 2 && y <= this.dy + this.sh /2;
+    let inside = insideX && insideY;
+  }
 }
